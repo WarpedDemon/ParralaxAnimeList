2acc38f48956b6c63127a890cddaaef8ee913d47
James Noonan <warped.demon.prince@hotmail.com>
2023-07-03 19:25:30 +0800

    Intial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ParralaxAnimeList
+# Parralax
  
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <meta name="description" content="Author: J.N. Author, Length: 1 pages">
+    <meta name="keywords" content="HTML, CSS, XML, JavaScript, Php, James">
+    <meta name="author" content="James Nicholas Noonan">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="type" content="website">
+    <meta property="site_name" content="mercinna">
+
+    <link rel="stylesheet" href="Css/LandingPage.css">
+    <link rel="stylesheet" href="Css/Mobile.css">
+    <link rel="stylesheet" href="Css/Extras.css">
+    <link rel="stylesheet" href="Css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <script src="Js/Master.mjs"></script>
+    <script src="Js/jquery.min.js"></script>
+    <script src="Js/wow.min.js"></script>
+    <script src="Js/jquery-ui.min.js"></script>
+
+    <title> Parralax is cool! </title>
+  </head>
+  <body>
+    <div class="parallax img1"></div>
+
+    <div class="parallax img2"></div>
+
+    <div class="parallax img3"></div>
+
+    <div class="parallax img4"></div>
+
+    <div class="parallax img5"></div>
+
+    <div class="parallax img6"></div>
+
+    <div class="parallax img7"></div>
+
+    <div class="parallax img8"></div>
+
+    <div class="parallax img9"></div>
+
+    <div class="parallax img10"></div>    
+
+    <video class="parallax video" controls>
+      <source src="https://parralaxissocool.herokuapp.com/video/101_idea_globe_slow.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  </body>
+</html>
